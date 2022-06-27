a978ca0f10cd7baa963cf6635ee3908c2f3e43c6
seungjjun <il1uso@naver.com>
2022-06-27 15:15:26 +0900

    메인페이지 기본 구성과 상단에 각 페이지의 게시글을 볼 수 있는 버튼 생성
    - 메뉴 버튼을 누르면 누른 버튼의 목적에 맞는 게시글을 볼 수 있도록 만듬

---
+++ Are-you-alone/src/main/java/utils/FutsalPageGenerator.java
@@ -0,0 +1,10 @@
+package utils;
+
+import javax.swing.*;
+
+public class FutsalPageGenerator extends JPanel {
+  public FutsalPageGenerator() {
+    JLabel label = new JLabel("6/26 17:00 메가테라 스타디움 팀원 모집");
+    this.add(label);
+  }
+}
